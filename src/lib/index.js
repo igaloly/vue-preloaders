cd2c77f2550beea0d7c75b7eb08720f479b51a2c
unknown <igaloly@gmail.com>
2019-11-17 21:04:39 +0200

    Handle close of never-opened container

--- src/lib/index.js
+++ src/lib/index.js
@@ -26,7 +26,7 @@ function preloaders(userDefaultOptions = {}) {
         open(userOptions = {}) {
             // if server - abort and return close callback (to save from throwing error firing the callback that comes from .open())
             if (process && process.server) {
-                return ()=>{};
+                return () => { };
             }
 
             // parse options
@@ -43,7 +43,7 @@ function preloaders(userDefaultOptions = {}) {
             let containerController = container[consts.loaderVariableName] || {};
 
             // get container's css position
-            const {position: containerCssPosition} = window.getComputedStyle(container)
+            const { position: containerCssPosition } = window.getComputedStyle(container)
             // set position change flag
             let isPositionChanged = !!containerController.isPositionChanged
             // if current position is static
@@ -57,7 +57,7 @@ function preloaders(userDefaultOptions = {}) {
             // if loader already initialized - if controller in container
             if (consts.loaderVariableName in container) {
                 // pass props if exists
-                if(options.props){
+                if (options.props) {
                     Object.entries(options.props).forEach(entry => {
                         const [key, value] = entry
                         containerController.props[key] = value
@@ -66,7 +66,7 @@ function preloaders(userDefaultOptions = {}) {
                 //set position changed flag
                 containerController.isPositionChanged = isPositionChanged
 
-            // if loader needs initialization
+                // if loader needs initialization
             } else {
                 // extend loader
                 const loaderConstructor = Vue.extend(loaderComponent);
@@ -100,14 +100,16 @@ function preloaders(userDefaultOptions = {}) {
 
             // return closing callback for loader
             return () => {
-                this.close({container})
+                this.close({ container })
             }
         },
-        close({container: userContainer} = {}) {
+        close({ container: userContainer } = {}) {
             // get container
             const container = helpers.getContainer(userContainer || defaultOptions.container);
             // get controller
-            const containerController = container[consts.loaderVariableName]
+            const containerController = container[consts.loaderVariableName];
+            // handle close of never-opened container
+            helpers.exeption.error('You must open a preloader before closing it.');
 
             // reset position after transition out
             setTimeout(() => {
