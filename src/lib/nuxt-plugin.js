32d1f35831e4eb436ec57f85d4b61cd907ef0a78
unknown <igaloly@gmail.com>
2019-11-18 21:24:08 +0200

    fix init HTML erorr

--- src/lib/nuxt-plugin.js
+++ src/lib/nuxt-plugin.js
@@ -2,5 +2,5 @@ import consts from '~/node_modules/vue-preloaders/src/consts';
 import preloaders from '~/node_modules/vue-preloaders/src/lib';
 
 export default (ctx, inject) => {
-    inject(consts.loaderInjectName, preloaders(JSON.parse(`<%= JSON.stringify(options).replace(/\\"/g, '\\"') %>`)))
+    inject(consts.loaderInjectName, preloaders(JSON.parse(`<%= JSON.stringify(options).replace(/\\"/g, '\\\\"') %>`)))
 }
